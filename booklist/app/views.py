91fcb334f335fb959fb75bafd3add6bdd8ccbb8e
Ismaxis <max200412345@gmail.com>
2022-07-06 18:02:43 +0300

    button works

--- booklist/app/views.py
+++ booklist/app/views.py
@@ -1,10 +1,25 @@
 import re
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Institution, Direction, List
 
 # Create your views here.
 
-
 def index(request):
-    return HttpResponse("Hello, world. You're at the app index.")
-    
+    dict = {
+        "urls": 'hum'
+    }
+    return render(request, 'app.html', dict)
+
+def hum(request):
+    q = Institution.objects.all()
+    dict = {
+        'dir_list': q,
+        }
+    return render(request, 'inst.html', dict)
+
+
+# def index(request):
+#     q = Institution.objects.all()
+#     dict = {'dir_list': q}
+#     return render(request, 'app.html', dict)
