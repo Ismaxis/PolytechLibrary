33e2673693c8af11989245aed9ad8b9061f4fa9d
Ismaxis <max200412345@gmail.com>
2022-07-06 17:15:27 +0300

    init

--- booklist/app/views.py
+++ booklist/app/views.py
@@ -6,5 +6,5 @@ from django.http import HttpResponse
 
 
 def index(request):
-    # return HttpResponse("Hello, world. You're at the app index.")'
+    return HttpResponse("Hello, world. You're at the app index.")'
     
